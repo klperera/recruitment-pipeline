2ab4d002e00939bfa17f14fcca65eb41a0ecd358
kalpa Perera <klperera660@gmail.com>
2025-05-17 00:26:40 +0530

    mapping endpoints in controller

---
+++ backend/src/main/java/org/example/backend/Controller/CandidateController.java
@@ -0,0 +1,48 @@
+package org.example.backend.Controller;
+
+import org.example.backend.Model.Candidate;
+import org.example.backend.Model.Stage;
+import org.example.backend.Service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/candidates")
+@CrossOrigin(origins = "")
+public class CandidateController {
+
+    private final CandidateService candidateService;
+
+    @Autowired
+    public CandidateController(CandidateService candidateService) {
+        this.candidateService = candidateService;
+    }
+
+    @GetMapping
+    public List<Candidate> getAllCandidates(@RequestBody(required = false)Stage stage) {
+        return stage == null? candidateService.getAllCandidates(stage) : candidateService.getAllCandidatesByStage(stage);
+    }
+
+    @GetMapping("/{id}")
+    public Candidate getById(@RequestBody Long id) {
+        return candidateService.getById(id);
+    }
+
+    @PostMapping
+    public Candidate createCandidate(@RequestBody Candidate candidate) {
+        return candidateService.createCandidate(candidate);
+    }
+
+    @PutMapping("/{id}")
+    public Candidate updateCandidate(@RequestBody Long id, @RequestBody Candidate updated) {
+        return candidateService.updateCandidate(id, updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCandidate(@RequestBody Long id) {
+        candidateService.deleteCandidate(id);
+    }
+
+}
