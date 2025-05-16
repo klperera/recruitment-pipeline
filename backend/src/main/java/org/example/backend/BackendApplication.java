6637bc9405b2deac35e5bee3b5a68547086e8285
kalpa Perera <klperera660@gmail.com>
2025-05-16 23:33:44 +0530

    Update BackendApplication.java

--- backend/src/main/java/org/example/backend/BackendApplication.java
+++ backend/src/main/java/org/example/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package org.example.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
         SpringApplication.run(BackendApplication.class, args);
     }
 
