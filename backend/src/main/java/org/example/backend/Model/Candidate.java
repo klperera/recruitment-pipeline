feef01a419750ea33a77675cb1efb6bf421a9970
kalpa Perera <klperera660@gmail.com>
2025-05-17 13:09:43 +0530

    getters and setters for candidate

--- backend/src/main/java/org/example/backend/Model/Candidate.java
+++ backend/src/main/java/org/example/backend/Model/Candidate.java
@@ -16,4 +16,51 @@ public class Candidate {
     private String referral;
     private String assessment;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+
+    public String getReferral() {
+        return referral;
+    }
+
+    public void setReferral(String referral) {
+        this.referral = referral;
+    }
+
+    public String getAssessment() {
+        return assessment;
+    }
+
+    public void setAssessment(String assessment) {
+        this.assessment = assessment;
+    }
 }
