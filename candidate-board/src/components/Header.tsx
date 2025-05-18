6164cb535ac27daeeb61138ea05beb33218a322d
kalpa Perera <klperera660@gmail.com>
2025-05-18 23:31:48 +0530

    Update Header.tsx

--- candidate-board/src/components/Header.tsx
+++ candidate-board/src/components/Header.tsx
@@ -1,6 +1,9 @@
 import "../css/Header.css";
+import { useState } from "react";
 
 function Header() {
+  const [tab, settab] = useState("Candidates");
+
   const job = {
     status: "Open",
     position: "Software Engineer",
@@ -37,13 +40,48 @@ function Header() {
         </div>
 
         <div className="nav-tabs">
-          <button className="nav-tab active">Candidates</button>
-          <button className="nav-tab">Job Info</button>
-          <button className="nav-tab">Calendar</button>
-          <button className="nav-tab">Score Card</button>
-          <button className="nav-tab">Activity</button>
-          <button className="nav-tab">Application Form</button>
-          <button className="nav-tab">Automation</button>
+          <button
+            className={`nav-tab ${tab === "Candidates" && "active"}`}
+            onClick={() => settab("Candidates")}
+          >
+            Candidates
+          </button>
+          <button
+            className={`nav-tab ${tab === "Job Info" && "active"}`}
+            onClick={() => settab("Job Info")}
+          >
+            Job Info
+          </button>
+          <button
+            className={`nav-tab ${tab === "Calendar" && "active"}`}
+            onClick={() => settab("Calendar")}
+          >
+            Calendar
+          </button>
+          <button
+            className={`nav-tab ${tab === "Score Card" && "active"}`}
+            onClick={() => settab("Score Card")}
+          >
+            Score Card
+          </button>
+          <button
+            className={`nav-tab ${tab === "Activity" && "active"}`}
+            onClick={() => settab("Activity")}
+          >
+            Activity
+          </button>
+          <button
+            className={`nav-tab ${tab === "Application" && "active"}`}
+            onClick={() => settab("Application")}
+          >
+            Application Form
+          </button>
+          <button
+            className={`nav-tab ${tab === "Automation" && "active"}`}
+            onClick={() => settab("Automation")}
+          >
+            Automation
+          </button>
         </div>
       </div>
     </>
